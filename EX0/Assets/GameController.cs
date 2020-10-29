9335615530096be12e97dc3fc970bd08c898bece
David <david.nir@intel.com>
2020-10-29 19:03:34 +0200

    Added ex0 files

---
+++ EX0/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static int FIELD_SIZE = 30; // Width and height of the game field
+    public static float COLLISION_THRESHOLD = 1.5f; // Collision distance between food and player 
+    public GameObject playerObject; // Reference to the Player GameObject
+
+    private GameObject food; // Represents the food in the game
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Positions the food at a random location inside the field
+    void SpawnFood()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ EX0/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementForce = 500; // Controls player movement power
+    private Rigidbody body;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Implement movement logic here
+    }
+}
