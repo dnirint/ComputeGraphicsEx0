a44c36631ca213cd08a60a047437b5cba1708b87
David <david.nir@intel.com>
2020-11-02 11:42:51 +0200

    Part2 complete

--- EX1/Ex1/Assets/CharacterAnimator.cs
+++ EX1/Ex1/Assets/CharacterAnimator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterAnimator : MonoBehaviour
 {
     public TextAsset BVHFile; // The BVH file that defines the animation and skeleton
@@ -17,19 +18,44 @@ public class CharacterAnimator : MonoBehaviour
         BVHParser parser = new BVHParser();
         data = parser.Parse(BVHFile);
         CreateJoint(data.rootJoint, Vector3.zero);
+
+
     }
 
     // Returns a Matrix4x4 representing a rotation aligning the up direction of an object with the given v
     Matrix4x4 RotateTowardsVector(Vector3 v)
     {
-        // Your code here
-        return Matrix4x4.zero;
+        Vector3 worldUpDirection = new Vector3(0, 1, 0);
+
+
+        //preparation
+       Vector3 normalizedV = v.normalized;
+
+        var angleX = -Mathf.Atan2(normalizedV[2], normalizedV[1]) * Mathf.Rad2Deg;
+        Matrix4x4 rotateX = MatrixUtils.RotateX(angleX);
+
+        var a = normalizedV[0];
+        var b = normalizedV[1];
+        var c = normalizedV[2];
+        var sqrtResult = Mathf.Sqrt(Mathf.Pow(c, 2) + Mathf.Pow(b, 2));
+        var angleZ = Mathf.Atan2(a, sqrtResult) * Mathf.Rad2Deg;
+        Matrix4x4 rotateZ = MatrixUtils.RotateZ(angleZ);
+
+        var generalRotate = rotateZ * rotateX;
+        var inverseGeneralRotate = generalRotate.inverse;
+
+        // todo DELETE THIS BEFORE SUBMISSION
+        Vector3 testRotateResult = inverseGeneralRotate.MultiplyVector(worldUpDirection);
+        bool verification = testRotateResult == v.normalized;
+        Debug.Assert(verification);
+
+        return inverseGeneralRotate;
     }
 
     // Creates a Cylinder GameObject between two given points in 3D space
     GameObject CreateCylinderBetweenPoints(Vector3 p1, Vector3 p2, float diameter)
     {
-        // Your code here
+        GameObject cylinderObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
         return null;
     }
 
@@ -42,11 +68,11 @@ public class CharacterAnimator : MonoBehaviour
     GameObject CreateJoint(BVHJoint joint, Vector3 parentPosition)
     {
         joint.gameObject = new GameObject(joint.name);
-        //joint.gameObject.transform.position = parentPosition;
+        
         GameObject sphereObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         sphereObject.transform.parent = joint.gameObject.transform;
 
-        Matrix4x4 scaleMatrix = (joint.name.Equals("Head")) ? scaleBy2Matrix : scaleBy8Matrix;
+        Matrix4x4 scaleMatrix = (joint.name.Equals("Head")) ? scaleBy8Matrix : scaleBy2Matrix;
         MatrixUtils.ApplyTransform(sphereObject, scaleMatrix);
 
         Matrix4x4 offsetToParent = MatrixUtils.Translate(joint.offset+parentPosition);
